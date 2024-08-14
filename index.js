72884331b2c891585c747ab1255255430bc39624
avalanche000 <125629389+avalanche000@users.noreply.github.com>
2024-08-14 01:13:25 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import * as canvasUtils from "./canvasUtils/index.js";
+import * as shapeUtils from "./shapeUtils/index.js";
+
+import * as DOMUtils from "./DOMUtils.js";
+import * as arrayUtils from "./arrayUtils.js";
+import * as numberUtils from "./numberUtils.js";
+
+export {
+  canvasUtils,
+  shapeUtils,
+  ...DOMUtils,
+  ...arrayUtils,
+  ...numberUtils,
+};
