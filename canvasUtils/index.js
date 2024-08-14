37120638bdc18f3e967f00eaf526b5b684e649a9
avalanche000 <125629389+avalanche000@users.noreply.github.com>
2024-08-14 01:14:55 -0400

    Create index.js

---
+++ canvasUtils/index.js
@@ -0,0 +1,11 @@
+import Canvas from "./Canvas.js";
+import DrawingUtils from "./DrawingUtils.js";
+import VectorCanvas from "./VectorCanvas.js";
+import VectorDrawingUtils from "./VectorDrawingUtils.js";
+
+export {
+  Canvas,
+  DrawingUtils,
+  VectorCanvas,
+  VectorDrawingUtils,
+};
