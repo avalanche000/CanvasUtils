8c7117ca79e706b13a1d7b82ae9519adedab1271
avalanche000 <125629389+avalanche000@users.noreply.github.com>
2024-08-14 01:16:21 -0400

    Create index.js

---
+++ shapeUtils/index.js
@@ -0,0 +1,11 @@
+import Rect from "./Rect.js";
+import Vector from "./Vector.js";
+import Vector2D from "./Vector2D.js";
+import VectorRect from "./VectorRect.js";
+
+export {
+  Rect,
+  Vector,
+  Vector2D,
+  VectorRect,
+};
