a21d77f55c83182f5139ca58e4d1ce36b8d7555c
avalanche000 <125629389+avalanche000@users.noreply.github.com>
2024-08-13 20:49:06 -0400

    Create Canvas.js

---
+++ canvasUtils/Canvas.js
@@ -0,0 +1,23 @@
+"use strict";
+
+import DrawingUtils from "./DrawingUtils.js";
+import Rect from "../shapeUtils/Rect.js";
+
+class Canvas {
+    constructor(DOMObject) {
+        this.DOMObject = DOMObject;
+        this.draw = new DrawingUtils(this);
+        this.rect = new Rect;
+
+        window.addEventListener("resize", () => this.resize());
+
+        this.resize();
+    }
+
+    resize() {
+        this.rect.width = this.DOMObject.width = window.innerWidth;
+        this.rect.height = this.DOMObject.height = window.innerHeight;
+    }
+}
+
+export default Canvas;
