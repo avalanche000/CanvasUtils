8cad469265d094dd70bd2a9cd74d4826ffbf3e2e
avalanche000 <125629389+avalanche000@users.noreply.github.com>
2024-08-19 01:15:31 +0000

    update createOptions description

--- functionUtils.js
+++ functionUtils.js
@@ -1,11 +1,10 @@
 "use strict";
 
 /**
- * (Linear Interpolate) Interpolates between two values a and b with t=0 being a and t=1 being b
- * @param {number} a - The value at t=0
- * @param {number} b - The value at t=1
- * @param {number} t - The interpolation value
- * @returns {number} The interpolated value
+ * Fills out the default option paramters with the inputted ones
+ * @param {object} default - The default options
+ * @param {object} options - The inputs uptions to be updated
+ * @returns {object} The default object with the options updated
  */
 function createOptions(defaults, options) {
     for (const key in options) {
